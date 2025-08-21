a0c984de87924cfb45b0b3e82d31640d73526ab7
Zent <azmienasir@usm.my>
2025-08-21 12:40:21 +0800

    10. Conditional Rendering

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const app = Vue.createApp({
     // data, functions
     data(){
         return {
+            showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
             age: '45'
         }
     },
     methods:{
-        changeTitle(title){
-            this.title = title
+        toggleShowBooks(){
+            this.showBooks = !this.showBooks
         }
     }
 })
